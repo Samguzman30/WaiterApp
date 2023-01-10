bdef29e9281776496d6371525c73d9c5f0f81912
Samguzman30 <19samuelguzman@gmail.com>
2023-01-10 00:00:57 -0300

    cambios

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.apps')
+
+@section('title', 'home')
+    
+@section('content')
+
+<h1 class="text-5xl text-center pt-24">Waiter App</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegisterController;
+USE App\Http\Controllers\SessionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
 });
