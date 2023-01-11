3e89338a034a5e0296aa24e77b6f679a434060b5
Samguzman30 <19samuelguzman@gmail.com>
2023-01-11 01:31:27 -0300

    cambios

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.apps')
+
+@section('title', 'Register')
+    
+@section('content')
+
+<h1 class="text-5xl text-black text-center pt-24">Register</h1>
+
+@endsection
--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -9,7 +9,14 @@
     href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
 
 </head>
-<body>
+<body class="bg-gray-100 text-gray-800" >
+<nav class="flex py-5 justify-start  text-black">
+<p class="justify-end">Waiter app</p>
+
+
+
+</nav>
+
     @yield('content')
 </body>
 </html>
