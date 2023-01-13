700ded6fbaec167c3097d50d1e0093923ada88d0
Samguzman30 <19samuelguzman@gmail.com>
2023-01-12 21:27:45 -0300

    cambios

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -7,6 +7,9 @@
     <title>@yield('title') - Waiter App</title>
     <link rel="stylesheet"
     href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+    <link rel="stylesheet" href="/resources/css/tailwind.css">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
+
 
 </head>
 <body class="bg-gray-100 text-gray-800" >
