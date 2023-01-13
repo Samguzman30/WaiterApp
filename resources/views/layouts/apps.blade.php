50d50595df3a895fcbec50ad8b2d0e8c7c21c28c
Samguzman30 <19samuelguzman@gmail.com>
2023-01-12 23:36:07 -0300

    cambios

--- resources/views/layouts/apps.blade.php
+++ resources/views/layouts/apps.blade.php
@@ -5,19 +5,30 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title') - Waiter App</title>
-    <link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.1/tailwind.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="/resources/css/tailwind.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800;900&family=Roboto&display=swap" rel="stylesheet">
+    <script>
+        tailwind.config = {
+          theme: {
+            extend: {
+                fontFamily: {
+                    'montserrat' : ['Montserrat', 'sans-serif']
+                }
+              colors: {
+                clifford: '#da373d',
+              }
+            }
+          }
+        }
+      </script>
 
+    
 
 </head>
 <body class="bg-gray-100 text-gray-800" >
-<nav class="flex py-5 justify-start  text-black">
-
-
-
-</nav>
 
     @yield('content')
 </body>
