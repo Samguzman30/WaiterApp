b84b1998791c2a1a025c14ae7a8f03747d97bd11
Samguzman30 <19samuelguzman@gmail.com>
2023-01-16 00:31:43 -0300

    cambios

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -10,7 +10,7 @@
           
           <!-- Card body -->
           <div class="relative px-4 sm:px-6 lg:px-8 pb-8 max-w-lg mx-auto" x-data="{ card: true }">
-              <div class="bg-white px-8 pb-6 rounded-2xl shadow-lg">
+              <div class="bg-white mt-10 mb-5 ml-8 mr-7 px-8 pb-6 rounded-2xl shadow-lg">
 
                   <!-- Card header -->
                   <div class="text-center  mb-6">
@@ -22,11 +22,12 @@
 
                   <!-- Toggle -->
                   <div class="flex  justify-center mb-6">
-                      <div class="relative flex w-full p-1 transition duration-150 border-b-8 border-purple-10 hover:transparent    rounded">
+                      <div class="relative flex w-full p-1 transition duration-150 border-b-4 border-[#3da496] border-purple-10 hover:transparent    rounded">
+                        
                           <span class="absolute inset-0 m-1 pointer-events-none" aria-hidden="true">
                               <span class="absolute inset-0 w-1/2  transform transition duration-150 ease-in-out" :class="card ? 'translate-x-0' : 'translate-x-full'"></span>
                           </span>
-                          <button class="relative flex-1 text-sm font-medium p-1 transition duration-150 ease-in-out focus:outline-none focus-visible:ring-2" @click.prevent="card = true">Ingresar</button>
+                          <button class=" relative flex-1 text-sm font-medium p-1 transition duration-150 ease-in-out focus:outline-none  focus-visible:ring-2" @click.prevent="card = true">Ingresar</button>
                           <button class="relative flex-1 text-sm font-medium p-1 transition duration-150 ease-in-out focus:outline-none  focus-visible:ring-2" @click.prevent="card = false">Registrarte</button>
                       </div>
                   </div>
@@ -53,7 +54,7 @@
                       <!-- Form footer -->
                       <div class="mt-6">
                           <div class="mb-4">
-                              <button class="font-medium text-sm inline-flex items-center justify-center px-3 py-2 border border-transparent rounded leading-5 shadow-sm transition duration-150 ease-in-out w-full bg-indigo-500 hover:bg-indigo-600 text-white focus:outline-none focus-visible:ring-2">Ingresar</button>
+                              <button class="font-medium text-sm inline-flex items-center justify-center px-3 py-2 border border-transparent rounded leading-5 shadow-sm transition duration-150 ease-in-out w-full bg-[#3da496] hover:bg-[#73cbbf] text-white focus:outline-none focus-visible:ring-2">Ingresar</button>
                           </div>
                       </div>
                   </div>
